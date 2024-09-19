cdaa6101665cab9f441d8cc8d20534eb11297c51
alisunnyday <alisonsun625@gmail.com>
2024-09-18 22:01:16 -0700

    some progress on functions

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,9 @@
 const questions = {
     "1": {
-        "question": "You and your friends are planning a fun picnic. What are you bringing?",
+        "question": "You and your flower friends are planning a fun picnic. What are you bringing?",
         // image
         "choices": {
-            "Utensils — everyone always forgets them!": [2, []],
+            "Utensils — everyone always forgets them!": [2, ["Lavender", ]],
             "A blanket — there would be no picnic without me": [2, []],
             "My famous": [2, []],
             "A bag of chips": [2, []],
@@ -16,21 +16,21 @@ const questions = {
         // image
         "choices": {
             "Well, goodbye world": [3, []],
-            "Position": [3, []],
+            "Try to remember what you read about it on the Internet": [3, []],
             "Scream for help!": [3, []],
-            "Frantically try to swim away": [3, []]
+            "Pray that there aren't rocks at the bottom": [3, []]
         }
     },
 
     "3": {
-        "question": "It's time to network! What's the move?",
+        "question": "You're at a big career event and it's time to network! What's the move?",
         // image
         "choices": {
-            "Utensils — everyone always forgets them!": [],
-            "A blanket — there would be no picnic without me": [],
-            "My famous": [],
-            "A bag of chips": [],
-            "Some drinks": []
+            "A": [],
+            "B": [],
+            "C": [],
+            "D": [],
+            "E": []
         }
     },
 
@@ -38,11 +38,11 @@ const questions = {
         "question": "You've just been granted immortality. Choose one thing to take with you throughout the rest of eternity.",
         // image
         "choices": {
-            "Utensils — everyone always forgets them!": [],
-            "A blanket — there would be no picnic without me": [],
-            "My famous": [],
-            "A bag of chips": [],
-            "Some drinks": []
+            "A": [],
+            "B": [],
+            "C": [],
+            "D": [],
+            "E": []
         }
     },
 
@@ -84,11 +84,63 @@ const questions = {
         }
     },
 
-    "8": {},
-    "9": {},
-    "10": {},
-    "11": {},
-    "12": {}
+    "8": {
+        "question": "Lorem Ipsum",
+        // image
+        "choices": {
+            "A": [],
+            "B": [],
+            "C": [],
+            "D": []
+        }
+    },
+
+    "9": {
+        "question": "Which personality trait do you value the most?",
+        // image
+        "choices": {
+            "Loyalty": [10, ["Rose"]],
+            "Empathy": [10, ["Rose"]],
+            "Creativity": [10, ["Bluebell"]],
+            "Ambition": [10, ["Tulip"]],
+            "Kindness": [10, ["Daisy"]],
+            "Compassion": [10, ["Sunflower"]],
+            "Honesty": [10, ["Lavender"]]
+        }
+    },
+
+    "10": {
+        "question": "Lorem Ipsum",
+        // image
+        "choices": {
+            "A": [],
+            "B": [],
+            "C": [],
+            "D": []
+        }
+    },
+
+    "11": {
+        "question": "Lorem Ipsum",
+        // image
+        "choices": {
+            "A": [],
+            "B": [],
+            "C": [],
+            "D": []
+        }
+    },
+
+    "12": {
+        "question": "Lorem Ipsum",
+        // image
+        "choices": {
+            "A": [],
+            "B": [],
+            "C": [],
+            "D": []
+        }
+    }
 
 }
 
@@ -102,20 +154,42 @@ const personalities = {
     "Baby's Breath": 0
 }
 
-let currentQuestionIndex = 0;       // what question are we on?
+let currentQuestionIndex = 1;       // what question are we on?
 
-function displayQuestion() {
-    const currentQuestion = questions[currentQuestionIndex];
+function displayQuestion(questionNum) {        // display each question and its choices
+    // const currentQuestion = questions[currentQuestionIndex];
     const questionText = document.getElementById('question-text');
-    const questionImage = document.getElementById('question-image');
+    // const questionImage = document.getElementById('question-image');
     const choicesContainer = document.getElementById('choices');
+
+    // const img = Image();         // image variables in const currently commented out bc they don't exist yet!!!
+    // img.src = questions[questionNum].image;
+    // questionImage.src = img.src;
+
+    questionText.textContent = questions[questionNum].question;
+
+    // create buttons for each question 
+    for (const [choice, info] of Object.entries(gameData[state].choices)) {
+        const button = document.createElement('button');
+        button.textContent = choice;
+        button.className = 'choice-button';
+        let nextState = info[0];
+        button.onclick = () => nextQuestion(nextState, info[1]); 
+        choicesContainer.appendChild(button);
+    };
 }
 
-function changeState() {
+function nextQuestion(selectedPersonalities) {           // log choice, move to next question, show result if at the end
+    // const currentQuestion = questions[currentQuestionIndex];
+
+    selectedPersonalities.forEach(personality => {
+        personalities[personality]++;
+    });
+
     currentQuestionIndex++;
 
     if (currentQuestionIndex < questions.length) {
-        displayQuestion();
+        displayQuestion(currentQuestionIndex);
     } 
     else {
         showResult();
@@ -124,8 +198,22 @@ function changeState() {
 
 function showResult() {
     // count flowers
+
     // load flower result img
-    // show flower result img
+    // const resultImage = document.getElementById('image');
+    // const resultPath = ``;
+    // const img = new Image();
+    // resultImage.src = resultPath;
+    // resultImage.className = 'responsive-image'; 
+    console.log("finish");
+
+    // sharing setup
+    const shareButton = document.createElement('button');
+    shareButton.textContent = 'Share with the rest of your garden!';
+    shareButton.className = 'choice-button';
+
+    // show flower result img: image onload plus text
+
     // share result api call
 
     // const shareData = {
@@ -149,4 +237,8 @@ function showResult() {
     //   });    
 }
 
-
+function startQuiz() {
+    currentQuestionIndex = 1;
+    // do some display stuff
+    displayQuestion(currentQuestionIndex)
+}
